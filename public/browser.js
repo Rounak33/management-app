e2dfe31d48fb9025d656043317e374f6584956ef
Rounak33 <rounak.rai33@gmail.com>
2023-04-02 22:45:12 +0530

    first commit

---
+++ public/browser.js
@@ -0,0 +1,13 @@
+
+  function register(){
+    window.location.href='/register';
+  }
+  function login(){
+    window.location.href='/login';
+  }
+  function forgotPassword(){
+    window.location.href='/forgotPasswordPage';
+  }
+  function resendVerificationMail(){
+    window.location.href='/resendVerificationMail';
+  }
